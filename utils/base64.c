02a7b5a76a81fef15481b75b28b6db9e69eca744
TheActualRen <mandaliadhiren@gmail.com>
2025-07-06 22:45:46 +0100

    got the base64 encoder working well

--- utils/base64.c
+++ utils/base64.c
@@ -3,23 +3,19 @@
 char table[65] = 
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	"abcdefghijklmnopqrstuvwxyz"
-	"0123456789+/";
+    "0123456789+/";
 
-
-int getsizeof(char* str) {
-	int size = 0;
-	int i = 0;
-	while (str[i] != '\0') {
-		size++;
-		i++;
+int getsizeof(const unsigned char* str) {
+	int len = 0;
+	while (str[len] != '\0') {
+		len++;
 	}
 
-	return size;
+	return len;
 }
 
-int binToDen(char* bin) {
-	int header = 32;
-	int value = 0;
+int binToDen(char *bin) {
+	int header = 32, value = 0;
 
 	for (int i = 0; i < 6; i++) {
 		if (bin[i] == '1') {
@@ -28,33 +24,26 @@ int binToDen(char* bin) {
 		header /= 2;
 	}
 
-	printf("Value: %d\n", value);
 	return value;
 }
 
-void charToBin(char c, char* bin, int idx) {
+void charToBin(char c, char *bin, int idx) {
 	int mask = 0b10000000;
 
 	for (int i = idx; i < idx + 8; i++) {
-		if (c & mask) {
-			bin[i] = '1';
-		} else {
-			bin[i] = '0';
-		}
+		bin[i] = c & mask ? '1' : '0';
 		mask >>= 1;
 	}
 }
 
-void encode(char *input) {
+void encode(const unsigned char *input) {
 	const int inputLen = getsizeof(input);
 
 	const int encodedLen = ((inputLen + 2) / 3) * 4;
 	const int totalBits = encodedLen * 6;
-
-	int padding = (3 - (inputLen % 3)) % 3;
+	const int padding = (3 - (inputLen % 3)) % 3;
 
 	char bin[totalBits + 1];
-
 	bin[totalBits] = '\0';
 
 	for (int i = 0; i < inputLen; i++) {
@@ -82,7 +71,8 @@ void encode(char *input) {
 }
 
 int main() {
-	encode("Hello World");
+	const unsigned char text[] = "";
+	encode(text);
 
 	return 0;
 }
