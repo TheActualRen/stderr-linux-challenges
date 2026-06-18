a5ea45711c563282cac249ad66fa2a1af2d9298d
TheActualRen <125380929+TheActualRen@users.noreply.github.com>
2026-06-18 18:21:30 +0100

    Delete utils directory

--- utils/gunzip/gunzip.h
+++
@@ -1,9 +0,0 @@
-#ifndef GUNZIP_H
-#define GUNZIP_H
-
-#include "deflate.h"
-
-void gunzip_encode(const char* input_path, const char* output_path, DeflateBlockType block_type);
-
-#endif
-
--- utils/gunzip/header.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_H
-#define HEADER_H
-
-#include <stdio.h>
-
-void write_gzip_header(FILE* out);
-
-#endif
